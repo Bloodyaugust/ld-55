2d9b824b6899b860699da9771973544878962eb7
Bloodyaugust <greysonrichey@gmail.com>
2024-04-13 19:55:35 -0600

    Add first art assets, it's towers!
    - Tweak camera settings

--- actors/tower.gd
+++ actors/tower.gd
@@ -1,9 +1,13 @@
 extends Node2D
 
+const ai_sprite: Texture2D = preload("res://sprites/tower_red.png")
+const player_sprite: Texture2D = preload("res://sprites/tower_blue.png")
+
 @export var data: TowerData
 @export var team: GameConstants.TEAM
 
 @onready var _health_bar: ProgressBar = %HealthBar
+@onready var _sprite: Sprite2D = %Sprite2D
 
 var _dead: bool = false
 var _health: float
@@ -22,3 +26,5 @@ func damage(amount: float) -> void:
 func _ready():
 	_health = data.health
 	_health_bar.value = _health / data.health
+	_sprite.texture = ai_sprite if team == GameConstants.TEAM.AI else player_sprite
+	
